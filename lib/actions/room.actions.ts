cfee06e0811e890637ca85e05ef05fb55b699f46
Flov9 <l.saihong25@gmail.com>
2024-08-29 17:38:25 +0800

    configure permissions in sharing feature

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -3,7 +3,7 @@
 import { nanoid } from 'nanoid'
 import { liveblocks } from '../liveblocks';
 import { revalidatePath } from 'next/cache';
-import { parseStringify } from '../utils';
+import { getAccessType, parseStringify } from '../utils';
 
 export const createDocument = async ({ userId, email }: CreateDocumentParams) => {
     const roomId = nanoid();
@@ -22,7 +22,7 @@ export const createDocument = async ({ userId, email }: CreateDocumentParams) =>
         const room = await liveblocks.createRoom(roomId, {
             metadata,
             usersAccesses,
-            defaultAccesses: ['room:write'],
+            defaultAccesses: [],
         });
 
         revalidatePath('/');
@@ -70,5 +70,45 @@ export const getDocuments = async (email: string) => {
             return parseStringify(rooms);
     } catch (error) {
         console.log(`There is an error while getting rooms: ${error}`);
+    }
+}
+
+export const updateDocumentAccess = async ({ roomId, email, userType, updatedBy }: ShareDocumentParams) => {
+    try {
+        const usersAccesses: RoomAccesses = {
+            [email]: getAccessType(userType) as AccessType, // [email] uses the email values as the keys
+        }
+
+        const room = await liveblocks.updateRoom(roomId, {
+            usersAccesses
+        })
+
+        if (room) {
+            // TODO: Send a notif to user
+        }
+        
+        revalidatePath(`/documents/${roomId}`)
+        
+        return parseStringify(room);
+    } catch (error) {
+        console.log(`Error happened while updating a room access: ${error}`)
+    }
+}
+
+export const removeCollaborator = async ({ roomId, email }: { roomId: string, email: string }) => {
+    try {
+        const room = await liveblocks.getRoom(roomId);
+
+        if (room.metadata.email === email) throw new Error('You cannot remove yourself from the document!');
+
+        const updatedRoom = liveblocks.updateRoom(roomId, {
+            [email]: null,
+        })
+
+        revalidatePath(`/documents/${roomId}`);
+
+        return parseStringify(updatedRoom);
+    } catch (error) {
+        console.log(`Error happened while removing a collaborator: ${error}`)
     }
 }
